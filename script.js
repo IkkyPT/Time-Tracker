650f09c5d73f2e4cfb6a291acf9486f0ca66ecd3
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-03-10 00:46:32 +0000

    Set reset() function - reset the timer

---
+++ script.js
@@ -0,0 +1,25 @@
+var hh = 0;
+var mm = 0;
+var ss = 0;
+
+function start(){
+}
+
+function reset(){
+    ss = 0;
+    mm = 0;
+    hh = 0;
+    document.getElementById('hour').innerHTML = "00:";
+    document.getElementById('minute').innerHTML = "00:";
+    document.getElementById('second').innerHTML = "00";
+}
+
+function stop(){
+
+}
+
+function timer(){
+    
+}
+
+
