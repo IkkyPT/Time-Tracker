f807aedc36cf49a613ee9cec2390a529ee3e22ae
IkkyPT <ricardoo1998carvalho@gmail.com>
2023-03-10 01:07:55 +0000

    Add timer function
    This commit adds a function called "timer" which is responsible for incrementing seconds, minutes, and hours and displaying them on the page. The function checks if the seconds have reached 60 and resets them to 0 while incrementing minutes. Similarly, the function checks if the minutes have reached 60 and resets them to 0 while incrementing hours. Finally, the function updates the HTML elements with the new values. The returnData function is used to format the numbers with leading zeros.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-var hh = 0;
-var mm = 0;
-var ss = 0;
+let hh = 0;
+let mm = 0;
+let ss = 0;
+let cron;
 
 function start(){
+    timer();
+    cron = setInterval(() => { timer(); }, 1000);
 }
 
 function reset(){
     ss = 0;
     mm = 0;
     hh = 0;
-    document.getElementById('hour').innerHTML = "00:";
-    document.getElementById('minute').innerHTML = "00:";
+    document.getElementById('hour').innerHTML = "00";
+    document.getElementById('minute').innerHTML = "00";
     document.getElementById('second').innerHTML = "00";
 }
 
@@ -19,7 +22,23 @@ function stop(){
 }
 
 function timer(){
-    
+    ss++;
+    if (ss == 60) {
+        ss = 0;
+        mm++;
+    if (mm == 60){
+        mm = 0;
+        hh++;
+        }
+    }
+
+    document.getElementById('hour').innerHTML = returnData(hh);
+    document.getElementById('minute').innerHTML = returnData(mm);
+    document.getElementById('second').innerHTML = returnData(ss);
+}
+
+function returnData(input) {
+    return input > 10 ? input : `0${input}`
 }
 
 
